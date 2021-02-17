e4c885f1b5537892f7c3b565bf81579f08bbc405
Carmen  Andra <carmennnnandra@gmail.com>
2021-02-17 19:01:39 +0200

    first commit

---
+++ account.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Gazdă: 127.0.0.1:3306:3306
+-- Timp de generare: feb. 03, 2021 la 03:03 PM
+-- Versiune server: 10.4.14-MariaDB
+-- Versiune PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Bază de date: `travel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structură tabel pentru tabel `account`
+--
+
+CREATE TABLE `account` (
+  `email` varchar(50) NOT NULL,
+  `password` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
